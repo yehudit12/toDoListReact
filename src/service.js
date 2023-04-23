8c73ff5af02817349715f722a2eb041a0a3a2327
yehudit12 <y7173245@gmail.com>
2023-04-23 20:06:02 +0300

    first commit

---
+++ src/service.js
@@ -0,0 +1,44 @@
+import axios from 'axios';
+
+const apiUrl = "http://localhost:5221/items"
+axios.defaults.baseURL= "http://localhost:5221/items"
+axios.interceptors.response.use(function (response) {
+  console.log("succesfuly")
+  // Any status code that lie within the range of 2xx cause this function to trigger
+  // Do something with response data
+  return response;
+}, function (error) {
+  console.log("error",error)
+  // Any status codes that falls outside the range of 2xx cause this function to trigger
+  // Do something with response error
+  return Promise.reject(error);
+});
+
+export default {
+  getTasks: async () => {
+    // const result = await axios.get(`${apiUrl}`)    
+    // return result.data;
+    const result = await axios.get()    
+    return result.data;
+  },
+
+  addTask: async(name)=>{
+    var item={Id:0,Name:name,IsComplete:false}
+    await axios.post('',item)
+    console.log('addTask', item)
+    //TODO
+    return {};
+  },
+
+  setCompleted: async(id, isComplete)=>{
+    await axios.put(`/${id}`,{IsComplete:isComplete})
+    console.log('setCompleted', {id, isComplete})
+    //TODO
+    return {};
+  },
+
+  deleteTask:async(id)=>{
+    await axios.delete(`/${id}`)
+    console.log('deleteTask')
+  }
+};
